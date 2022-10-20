d13a8febc1ce153b12479d36b4402235b1b19427
andanilov <andi.danilov@gmail.com>
2022-10-20 09:32:56 +0300

    add an interceptor for updating access token

--- src/app/http/api.ts
+++ src/app/http/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import appConfig from '../../config.json';
 import StorageService from '../services/StorageService';
+import AuthService from '../services/AuthService';
 
 const api = axios.create({
   withCredentials: true, // Add cookie to every request
@@ -11,6 +12,20 @@ const api = axios.create({
 api.interceptors.request.use((config: AxiosRequestConfig) => {
   config?.headers && (config.headers.Authorization = `Bearer ${StorageService.getAccesToken()}`);
   return config;
+});
+
+// If we have code 401 we should send request for new access token and repeat original request
+api.interceptors.response.use((config) => config, async (error) => {
+  const originalRequest = error.config;
+  if (error.response.status === 401) {
+    try {
+      const userData = await AuthService.checkAuth();
+      StorageService.setAccesToken(userData.accessToken);
+      return api.request(originalRequest);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 });
 
 export default api;
